238fdd526c601ed37bfac8997edebf87c50c3c52
Jaime <hanndel31@gmail.com>
2023-02-06 11:57:07 +0100

    nose

---
+++ Desarrollo de interfaces/Tema 8/MauiApp1VS/MauiApp1VS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1VS;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Desarrollo de interfaces/Tema 8/MauiApp1VS/MauiApp1VS/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1VS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
